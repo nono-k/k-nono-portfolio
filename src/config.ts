22bca53e7255af0cfec15acee6990454499599e1
iwasaki <poettype73@gmail.com>
2025-05-28 08:59:52 +0900

    Fix config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,18 +1,18 @@
-import type { SiteConfig, HeaderLink } from "@/types/config";
+import type { HeaderLink, SiteConfig } from '@/types/config';
 
 export const siteConfig: SiteConfig = {
-  siteTitle: "K-Nono Portfolio",
-  siteDesc: "My Site Description",
-  siteUrl: "https://example.com",
-  siteType: "website",
-  siteLocale: "ja_JP",
-  siteIcon: "/favicon.ico",
-  siteImg: "/ogp.png",
-}
+  siteTitle: 'K-Nono Portfolio',
+  siteDesc: 'My Site Description',
+  siteUrl: 'https://example.com',
+  siteType: 'website',
+  siteLocale: 'ja_JP',
+  siteIcon: '/favicon.ico',
+  siteImg: '/ogp.png',
+};
 
 export const headerLink: HeaderLink[] = [
+  { name: 'home', url: '/' },
   { name: 'about', url: '/about' },
   { name: 'work', url: '/work' },
-  { name: 'demo', url: '/demo' },
-  { name: 'contact', url: '/contact' },
-]
+  { name: 'sketch', url: '/sketch' },
+];
